f46e130dbdd3a279d515d2e5fbce24e1f1820cc6
Svetlana_Lapshyna <svetlana.lapshyna@gmail.com>
2021-03-09 12:48:18 +0300

    Check the Presence of Elements for PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -4,9 +4,13 @@ import time
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
-    print("\nInit browser")
+    print("\nInitiate Browser!")
     page.open()
-    print("\nOpen site")
+    print("\nSite is opened!")
+   
+    page.should_be_login_link()
+    print("\nChecked Login Page!")
+    
     page.go_to_login_page()
-    print("\nLoaded Login Page")
+    print("\nLoaded Login Page!")
     
