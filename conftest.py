a6909861556ebbbf33bddec5ab60b76adf93f13a
Svetlana_Lapshyna <svetlana.lapshyna@gmail.com>
2021-03-09 11:41:18 +0300

    Refactoring with PageObject in pytest_project

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--user_language', action='store', default='ru',
+    parser.addoption('--user_language', action='store', default='en',
                      help="Choose language: RU or EN")
 
 @pytest.fixture(scope="function")
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,12 @@
+from .pages.main_page import MainPage
 import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    time.sleep(3)
-    login_link.click()
+    page = MainPage(browser, link)
+    print("\nInit browser")
+    page.open()
+    print("\nOpen site")
+    page.go_to_login_page()
+    print("\nLoaded Login Page")
+    
